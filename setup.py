5c9c15b5dca74de712888d1e8c6e6926447073e9
mvr <matthewvonrocketstein@gmail-dot-com>
2011-08-20 15:30:22 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+""" setup.py for report
+"""
+
+import setuptools
+from distutils.core import setup
+
+setup(
+    name         ='report',
+    version      = '.1',
+    description  = 'patterns in reporting',
+    author       = 'mvr',
+    url          = 'https://github.com/mattvonrocketstein/reporting',
+    py_modules   = ['report'],
+)
