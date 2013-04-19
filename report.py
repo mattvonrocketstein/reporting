3617b46b3f992a929c006908037f025e2f262d58
matt <mvr@laptop>
2013-04-19 12:46:22 -0400

    cleanup; support for detecting console width

--- report.py
+++ report.py
@@ -1,46 +1,16 @@
 """ report.py
 """
-
-"""
-import logging
-
-LOGGERS = {}
-# create logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-
-# 'application' code
-logger.debug('debug message')
-logger.info('info message')
-logger.warn('warn message')
-logger.error('error message')
-logger.critical('critical message')
-
-"""
 import os
 import sys
 import inspect
-
+from collections import namedtuple
+from datetime import datetime, timedelta
 import pygments
 from pygments import highlight
 from pygments.lexers import PythonLexer, PythonTracebackLexer
 from pygments.formatters import HtmlFormatter, Terminal256Formatter
 from pygments.console import colorize as console_color
-
+from pygments.console import codes as console_codes
 
 plex  = PythonLexer()
 tblex = PythonTracebackLexer()
@@ -48,19 +18,33 @@ hfom  = HtmlFormatter()
 hfom2 = HtmlFormatter(cssclass="autumn")
 colorize  = lambda code: highlight(code, plex, hfom)
 colorize2 = lambda code: highlight(code, plex, hfom2)
+ROW_LEN_CACHE = dict(timestamp=None, stdout_row_length=None)
 
 if not sys.stdout.isatty():
-    console_color = lambda name,string: string
+    # let's not work too hard if there is no one
+    # to notice all the color
+    console_color = lambda name, string: string
+    stdout_row_length = lambda: 80
+else:
+    def stdout_row_length():
+        """ returns the number of cols in the display.
+            this is cached for CACHE_LENGTH amount of time,
+            but after that it's recomputed.  this allows for
+            terminal windows that are getting resized to behave
+            as expected
+        """
+        if ROW_LEN_CACHE['stdout_row_length'] is None or \
+           ROW_LEN_CACHE['timestamp'] < (datetime.now()-timedelta(seconds=10)):
+            try:
+                cols, rows = os.popen('stty size', 'r').read().split()
+                rows = int(rows)
+            except:
+                rows = 80
+            ROW_LEN_CACHE['stdout_row_length'] = int(rows)
+            ROW_LEN_CACHE['timestamp'] = datetime.now()
+        return ROW_LEN_CACHE['stdout_row_length']
 
-from pygments.console import codes as console_codes
-#c_codes = console_codes.items()
-#c_codes.sort(lambda x,y: cmp(len(x[1]),len(y[1])))
-#c_codes.reverse()
 def console2html(txt):
-    #txt = txt.replace(console_codes['reset'],'</font>')
-    #for name, code in c_codes:
-    #    if all([name, name!='reset']):
-    #        txt=txt.replace(code, '<font color="{0}">'.format(name))
     return txt.replace('\n', '<br/>')
 
 class console:
@@ -119,6 +103,7 @@ def whoami():
     return inspect.stack()[1][3]
 
 def getcaller(level=2):
+    """ """
     x = inspect.stack()[level]
     frame = x[0]
     file_name = x[1]
@@ -131,19 +116,21 @@ def getcaller(level=2):
     if type(kls_func)==property:
         func = kls_func
     else:
-        try: func = self and getattr(self,func_name)
-        except AttributeError: func = func_name+'[nested]'
+        try:
+            func = self and getattr(self, func_name)
+        except AttributeError:
+            func = func_name+'[nested]'
     return dict(file=file_name,
                 kls=kls,
                 self=self,
                 func=func,
                 func_name=func_name)
 
-def whosdaddy():
+def whosdaddy(frames_back=3):
     """ displays information about the caller's caller """
     # if self is a named argument in the locals, print
     # the class name, otherwise admit that we don't know
-    caller_info = getcaller(3)
+    caller_info = getcaller(frames_back)
     kls         = caller_info['kls']
     file_name   = caller_info['file']
     func_name   = caller_info['func_name']
@@ -152,15 +139,19 @@ def whosdaddy():
     file_parts  = file_name.split(os.path.sep)
     if len(file_parts) > 4:
         file_name = os.path.sep.join(file_parts[-4:])
-    return ' + ' + console.darkblue(file_name) + ' --  ' + console.blue(header)
+    return file_name,header
+    #return ' + ' + console.darkblue(file_name) + ' --  ' + console.blue(header)
 
 def report(*args, **kargs):
     """ reporting mechanism with inspection and colorized output """
     stream = kargs.pop('stream', sys.stdout)
+    frames_back = kargs.pop('frames_back', 3)
     header = kargs.pop('header', '')
-    header=whosdaddy();
-    print header
-
+    #_header = whosdaddy(frames_back);
+    fname, caller = whosdaddy(frames_back)
+    colored_header = ' ' + console.darkblue(fname) + ' --  ' + console.blue(caller)
+    extra_length = len(' + '+' --  ') #ugh
+    header_length = len(fname+caller)+extra_length
     if len(args)==1:
             _args = str(args[0])
             # if kargs appears to be a formatting string for the one and only
@@ -172,14 +163,29 @@ def report(*args, **kargs):
                 except KeyError:
                     pass
                 kargs = {}
-            _args = console.darkteal(_args.strip())
+            _args = _args.strip()
+            # whenever terminal is wide enough to show both,
+            # mash up the header with the other output
+            if (len(_args+' -- ') + header_length) < stdout_row_length():
+                _header = colored_header + ' -- ' + console.darkteal(_args)
+                _args = ''
+            else:
+                _args = '  '+console.darkteal(_args)
+                _header = colored_header
     else:
-            _args = 'args=' + console.color(str(args)).strip()
+        from pprint import pprint
+        from StringIO import StringIO
+        s = StringIO()
+        tmptmp = pprint(args, s)
+        s.seek(0); tmptmp=s.read()
+        _args = 'args=' + console.color(tmptmp).strip()+'\n'
+    print _header
     _args = _args + '\n' if _args else _args
     _kargs =  console.color(str(kargs)) if kargs else ''
     _kargs = _kargs +'\n' if _kargs else _kargs
-    sep = '    '
-    stream.write( sep + _args + sep + _kargs)
+    sep = ' '
+    output= sep + _args + sep + _kargs
+    stream.write(output)
     stream.flush()
 
 def getReporter(**unused):
